c57dc6baf1297e9c0ed93636304a3ec0aa1823e0
BoneBot <cmcbride0304@gmail.com>
2025-07-30 23:44:40 -0500

    Add all the sprites

--- project/game/map/obstacles/sentry.gd
+++ project/game/map/obstacles/sentry.gd
@@ -18,6 +18,7 @@ extends Area2D
 @export var turn_time := 1.0
 
 @onready var sentry: Area2D = $"."
+@onready var sprite: AnimatedSprite2D = $Sprite
 
 # Used to flip the turn direction when the turn type is Alternating
 var flip_turn := false
@@ -28,9 +29,25 @@ func _ready() -> void:
 		field_of_view.visible = fov_visible
 
 
-func _on_body_entered(_body: Node2D) -> void:
-	#print("Player detected!")
+func _process(delta: float) -> void:
+	# I couldn't get the sprite to rotate properly. Maybe one day it will. RIP 
 	pass
+	#var rotation = fmod(rotation_degrees, 360)
+	#if rotation < 0:
+		#rotation += 360
+	#
+	#var new_animation = "down"
+	#if rotation >= 45 and rotation < 135:
+		#new_animation = "left"
+	#elif rotation >= 135 and rotation < 225:
+		#new_animation = "up"
+	#elif rotation >= 225 and rotation < 315:
+		#new_animation = "right"
+	#else:
+		#new_animation = "down"
+	#
+	#if sprite.animation != new_animation:
+		#sprite.play(new_animation)
 
 
 func show_fov() -> void:
